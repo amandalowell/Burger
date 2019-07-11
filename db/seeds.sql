4df425e650abc7e4654e5f17d22346eb8b3ff794
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-11 18:52:58 -0500

    db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tacoburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cochiburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bombaburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Presiburger', false);
