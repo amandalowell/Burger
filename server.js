6fab1583c58b1534e98a29043287bbcaaca8cf9c
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-11 23:29:13 -0500

    ryley

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 
-
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 
 var exphbs = require("express-handlebars");
@@ -20,7 +23,7 @@ app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+app.use("/",routes);
 
 
 app.listen(PORT, function() {
