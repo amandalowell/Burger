e527aaf05aa6b303bc454503dbc83240de498be7
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-11 21:11:11 -0500

    portnum

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
 } else {   
    connection = mysql.createConnection({
    host: "localhost",
-   port:3306,
+   port:8080,
    user:"root",
    password:"rootroot",
    database:"burgers_db"
