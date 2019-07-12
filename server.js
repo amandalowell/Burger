2d55fdf5a221162106eb439ea1c4201dc45e1324
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-11 23:25:15 -0500

    server back

--- server.js
+++ server.js
@@ -1,37 +1,29 @@
-//This file is used to store the connection to the server
-//Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var exphbs = require('express-handlebars');
- //Import routes
-var routes = require('./controllers/burgers_controller.js');
+var express = require("express");
 
-// Express set up
+var PORT = process.env.PORT || 8080;
+
 var app = express();
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
-
-// Parse application body as JSON
-app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-//
 
 
-//
-app.use("/", routes);
-
-// Set Handlebars.
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.use(express.static("public"));
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-var PORT = process.env.PORT ||8080;
 
-// Start our server so that it can begin listening to client requests.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
+  
   console.log("Server listening on: http://localhost:" + PORT);
 });
